2921ec65c7a4da8d541a2fd0167f4c3f2507d3ef
Swatijoshi2002 <79635662+Swatijoshi2002@users.noreply.github.com>
2021-12-23 10:05:21 +0530

    Update hfh.c

--- hfh.c
+++ hfh.c
@@ -1,17 +1,16 @@
-
 #include <stdio.h>
+#include<stdlib.h>
 struct node{
     int data;
-    int weight;
-    int next;
+    //int weight;
+    struct node* next;
 };
 int main()
 { 
-    struct node* arr[10]=NULL;
+    struct node* arr[10]={NULL};
     while(1)
     { 
         int n;
-        scanf("%d",&n);
         printf("enter 1 to add edge\n 0 to end the graph\n");
         scanf("%d",&n);
         if(n==1)
@@ -20,19 +19,39 @@ int main()
     int v;
     int e;
     scanf("%d",&v);
+      printf("enter second edge and weight\n");
+        scanf("%d",&e);
+       // int w;
+       // scanf("%d",&w);
+     struct node*temp=(struct node*)malloc(sizeof(struct node));
+        temp->data=e;
+      //  temp->weight=w;
+        temp->next=NULL;
     if(arr[v]==NULL)
     {
-        struct node*temp=(struct node*)malloc(sizeof(struct node));
-        printf("enter second edge and weight\n");
-        scanf("%d",&e);
-        int w;
-        scanf("%d",&w);
-        temp->data=e;
-        temp->weight=w;
-        temp->next=NULL;
+        arr[v]=temp;
     }
+    else {temp->next=arr[v];
+        arr[v]=temp;
+       }
         }
-        else break;
+         else break;
+    }
+    
+for(int i=0;i<10;i++)
+{
+    struct node* temp=arr[i];
+    if(temp==NULL)
+    continue;
+    else{
+    printf("edge through %d=",i);
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+    }
     
 }
-
+}
